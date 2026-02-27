b85f9fb347f1750282eb00ede08eb50d7805fc3a
“nguyenvailon” <nguyendeptrai812@gmail.com>
2026-02-27 22:56:16 +0700

    Update: Component, Arrow Function

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+
+const MyComponent = () => {
+  return (
+    <div>Hello</div>
+  );
+}
 const App = () => {
   const [count, setCount] = useState(0)
 
@@ -23,6 +29,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello Word</h1>
+      <MyComponent></MyComponent>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
