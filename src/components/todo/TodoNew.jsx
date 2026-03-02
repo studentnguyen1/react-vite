b7375d857956ac37239093ff7cbee57c37ed0611
“nguyenvailon” <nguyendeptrai812@gmail.com>
2026-03-02 10:39:21 +0700

    Using DOM Event in React

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,17 @@
 const TodoNew = (props) => {
-    const { newFunction } = props;
-    newFunction("nguyen")
+    // const { newFunction } = props;
+    // newFunction("nguyen")
+    const hanldeButton = () => {
+        alert("Click me")
+    }
+    const handleOnChange = (name) => {
+        console.log("Input is: ", name)
+
+    }
     return (
         <div className="todo-new">
-            <input type="text" />
-            <button>Add</button>
+            <input type="text" onChange={(event) => handleOnChange(event.target.value)} />
+            <button style={{ cursor: "pointer" }} onClick={hanldeButton} >Add</button>
         </div>
     )
 }
