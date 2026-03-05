1b5050ec4fff80e0f7dcac4d0faf1ebb7d6fa384
“nguyenvailon” <nguyendeptrai812@gmail.com>
2026-03-05 22:18:40 +0700

    Add router to Devide Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <div>Login page</div>,
+  },
+  {
+    path: "/register",
+    element: <div>Register page</div>,
+  },
+  {
+    path: "/users",
+    element: <div>Users page</div>,
+  },
+  {
+    path: "/product",
+    element: <div>Product page</div>,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
